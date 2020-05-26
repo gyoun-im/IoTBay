7caf28258ddd0ea9ebd9655c47912fa9d7624db9
gabzorz <gabz_090@hotmail.com>
2020-05-26 15:11:44 +1000

    all tables populated

--- IoTBay/db/InsertValues/insertStaffTable.sql
+++ IoTBay/db/InsertValues/insertStaffTable.sql
@@ -1,1 +1,24 @@
-
+INSERT INTO STAFF
+(NAME, EMAIL, PHONENUMBER, ADDRESS, STAFFTYPE)
+VALUES
+('Sarah Grant', 'sarahgrant@gmail.com', '0478999656', '14 Mellow St Sydney 2000', 'Normal staff'),
+('Ben Malik', 'benmalik@gmail.com', '0466332230', '26 Grange St Sydney 2000', 'Normal staff'),
+('Philip Saw', 'philipsaw@gmail.com', '0411232332', '28 Red St Sydney 2000', 'Normal staff'),
+('Mark Trobe', 'marktrobe@gmail.com', '0489988895', '23 Guard St Sydney 2000', 'Manager'),
+('David So', 'davidso@gmail.com', '0415323656', '56 Bright St Sydney 2000', 'Normal staff'),
+('Ana Label', 'analabel@gmail.com', '0478977888', '12 Light St Sydney 2000', 'Normal staff'),
+('Joyce Reyes', 'joycereyes@gmail.com', '0444111555', '10 Dark St Sydney 2000', 'Manager'),
+('Kiara Daves', 'kiaradaves@gmail.com', '0456332225', '12 Block St Sydney 2000', 'Normal staff'),
+('Nick Plays', 'nickplays@gmail.com', '0455623147', '15 Tick St Sydney 2000', 'Normal staff'),
+('Freya Gold', 'freyagold@gmail.com', '0453214987', '58 Screen St Sydney 2000', 'Manager'),
+('Amanada Rows', 'amandarows@gmail.com', '0411122369', '8 Boxes St Sydney 2000', 'Normal staff'),
+('Darlene Furlow', 'darlenefurlow@gmail.com', '0465789456', '9 Keys St Sydney 2000', 'Normal staff'),
+('Julie Joys', 'juliejoys@gmail.com', '0412123456', '56 Mellow St Sydney 2000', 'Normal staff'),
+('Trevor David', 'trevordavid@gmail.com', '0465741852', '90 Yellow St Sydney 2000', 'Manager'),
+('Mary Green', 'marygreen@gmail.com', '0496852963', '56 Dark St Sydney 2000', 'Normal staff'),
+('Blake Dark', 'blakedark@gmail.com', '0498654321', '10 Bright St Sydney 2000', 'Normal staff'),
+('Stan Proper', 'stanproper@gmail.com', '0478963123', '11 Soldier St Sydney 2000', 'Normal staff'),
+('Chris Star', 'chrisstar@gmail.com', '0456123789', '7 Table St Sydney 2000', 'Normal staff'),
+('Niel Diamond', 'nieldiamond@gmail.com', '0496159753', '1 Chair St Sydney 2000', 'Normal staff'),
+('Aaron Frey', 'aaronfrey@gmail.com', '0496946156', '10 Block St Sydney 2000', 'Normal staff'),
+('Fiona Al', 'fionaal@gmail.com', '0478753159', '63 Mellow St Sydney 2000', 'Normal staff');
