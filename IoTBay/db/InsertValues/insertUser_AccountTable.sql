4c617a38d420a3abe0655eac927b7d89e5ccfa28
gabzorz <gabz_090@hotmail.com>
2020-05-26 12:34:54 +1000

    added new inserts

---
+++ IoTBay/db/InsertValues/insertUser_AccountTable.sql
@@ -0,0 +1,24 @@
+INSERT INTO USER_ACCOUNT
+(USERNAME, PASSWORD, DATEOFBIRTH, GENDER, PROMOTIONALNEWSLETTER, REWARDPOINTS)
+Values
+('susangretch@gmail.com', 'hello', '31/12/1995', 'Female', false, 0),
+('albertprob@gmail.com', 'wave', '15/02/1995', 'Male', true, 10),
+('annamel@gmail.com', 'loppy', '16/08/1995', 'Female', false, 30),
+('sarahtrumb@gmail.com', 'blue', '02/02/1997', 'Female', false, 50),
+('alextreck@gmail.com', 'turtle', '03/01/1994', 'Male', false, 0),
+('tomtreb@gmail.com', 'mountain', '26/06/1995', 'Male', true, 0),
+('mallorylurd@gmail.com', 'forest', '14/08/1995', 'Male', false, 20),
+('lydiashield@gmail.com', 'shield', '16/08/1989', 'Female', false, 0),
+('philipcruz@gmail.com', 'pool', '13/07/1996', 'Male', true, 0),
+('kylieknows@gmail.com', 'sunny', '08/08/1995', 'Female', true, 50),
+('petersands@gmail.com', 'house', '28/02/1990', 'Male', true, 0),
+('danielmaloy@gmail.com', 'grass', '12/09/1995', 'Male', false, 0),
+('sophieraven@gmail.com', 'window', '17/07/1993', 'Female', true, 80),
+('vickylast@gmail.com', 'shiny', '25/07/1995', 'Female', false, 80),
+('williamhigh@gmail.com', 'class', '14/07/1995', 'Male', false, 0),
+('benwallow@gmail.com', 'drawer', '06/06/1995', 'Male', true, 0),
+('mantomb@gmail.com', 'container', '04/04/1990', 'Male', false, 10),
+('kennedyfrast@gmail.com', 'roads', '05/05/1995', 'Male', true, 10),
+('lizlips@gmail.com', 'boxes', '19/08/1995', 'Female', true, 0),
+('shanebran@gmail.com', 'keys', '14/07/1996', 'Male', false, 0),
+('philbranson@gmail.com', 'rakes', '15/08/1990', 'Female', false, 20);
