fee3c93ebeeb10792c24e4347e5c0e41c75d8cb9
gabzorz <gabz_090@hotmail.com>
2020-06-01 20:38:15 +1000

    m

--- IoTBay/src/java/uts/isd/controller/LogoutController.java
+++ IoTBay/src/java/uts/isd/controller/LogoutController.java
@@ -3,12 +3,14 @@ package uts.isd.controller;
 
 import java.io.IOException;
 import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LogoutController {
+public class LogoutController extends HttpServlet{
     
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         HttpSession session = request.getSession();
         session.invalidate();   //delete the session
