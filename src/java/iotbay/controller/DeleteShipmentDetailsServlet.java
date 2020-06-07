f77ccae9c750b0e1d745b1a008a08abb65da8a83
velderome <54608051+velderome@users.noreply.github.com>
2020-06-08 04:26:25 +1000

    changed package name

--- src/java/iotbay/controller/DeleteShipmentDetailsServlet.java
+++ src/java/iotbay/controller/DeleteShipmentDetailsServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package iotbay.controller;
+package uts.isd.controller;
 
 import iotbay.model.dao.ShipmentDBManager;
 import java.io.IOException;
