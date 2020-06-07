796c46cac3b9c7e48ea413eb5432f7ff2fe8337c
velderome <54608051+velderome@users.noreply.github.com>
2020-06-08 04:27:33 +1000

    Update ShipmentServlet.java

--- src/java/iotbay/controller/ShipmentServlet.java
+++ src/java/iotbay/controller/ShipmentServlet.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package iotbay.controller;
+package uts.isd.controller;
 
-import iotbay.model.Shipment;
-import iotbay.model.Shipment_Details;
-import iotbay.model.dao.ShipmentDBManager;
+import uts.isd.model.Shipment;
+import uts.isd.model.Shipment_Details;
+import uts.isd.model.dao.ShipmentDBManager;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
