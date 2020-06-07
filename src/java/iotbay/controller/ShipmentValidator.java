e2b605023693d860dbbb0b301db6bdaf79d80e62
velderome <54608051+velderome@users.noreply.github.com>
2020-06-08 04:29:18 +1000

    Update ShipmentValidator.java

--- src/java/iotbay/controller/ShipmentValidator.java
+++ src/java/iotbay/controller/ShipmentValidator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package iotbay.controller;
+package uts.isd.controller;
 
 import java.io.Serializable;
 import java.util.regex.Matcher;
